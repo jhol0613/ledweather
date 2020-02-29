f8b1b438eac5c2b03eee55c8dc73322e918dc9f2
John Holland <john.hollandr@gmai.com>
2020-02-29 16:41:13 -0600

    first commit

---
+++ metar.py
@@ -0,0 +1,25 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+
+with open("/home/ledweather/airports") as f:
+	airports = f.readlines()
+airports = [x.strip() for x in airports]
+
+# Retrieve METAR from aviationweather.gov data server
+# Details about parameters can be found here: https://www.aviationweather.gov/dataserver/example?datatype=metar
+url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=5&mostRecentForEachStation=true&stationString=" + ",".join([item for item in airports if item != "NULL"])
+print(url)
+
+content = urllib.request.urlopen(url).read()
+
+# Retrieve flying conditions from the service response and store in a dictionary for each airport
+root = ET.fromstring(content)
+conditionDict = { "":"" }
+for metar in root.iter('METAR'):
+	stationId = metar.find('station_id').text
+	if metar.find('flight_category') is None:
+		print("Missing flight condition, skipping.")
+		continue
+	flightCategory = metar.find('flight_category').text
+	conditionDict[stationId] = flightCategory
+	print(flightCategory)
