0265ef497ad32e49fd769b571cf274cf48bf6f62
John Holland <john.hollandr@gmai.com>
2020-03-02 19:54:11 -0600

    bleh

--- metar.py
+++ metar.py
@@ -1,6 +1,8 @@
 import urllib.request
 import xml.etree.ElementTree as ET
 import colors
+import board
+import neopixel
 
 with open("airports.txt") as f:
 	airports = f.readlines()
@@ -21,11 +23,33 @@ for metar in root.iter('METAR'):
 	stationId = metar.find('station_id').text
 	if metar.find('flight_category') is None:
 		print("Missing flight condition, skipping.")
+		conditionDict[stationId] = "Error"
 		#print("Missing flight condition for " + stationId)
 		continue
 	flightCategory = metar.find('flight_category').text
 	conditionDict[stationId] = flightCategory
 	#print("" + stationId + ": " + flightCategory)
 
-for airportCode in airports:
+pixels = neopixel.NeoPixel(board.D18, conditionDict.length)
+
+for i, airportCode in enumerate(airports):
+	if flightCategory != "No":
+		if flightCategory == "VFR":
+			color = colors.VFR
+		elif flightCategory == "MVFR":
+			color = colors.MVFR
+		elif flightCategory == "IFR":
+			color = colors.IFR
+		elif flightCategory == "LIFR":
+			color = colors.LIFR
+		else:
+			color = colors.ERROR
+
+	pixels[i] = color
 	print ("" + airportCode + ": " + conditionDict[airportCode])
+
+
+
+
+
+
