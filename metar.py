896902e15b8d80f2fbd6202cc72795e6b8022bcf
John Holland <john.hollandr@gmai.com>
2020-03-10 20:47:56 -0500

    refresh

--- metar.py
+++ metar.py
@@ -9,6 +9,8 @@ COLOR_IFR = (0,50,0)
 COLOR_LIFR = (0,50,50)
 COLOR_ERROR = (20,20,20)
 
+REFRESH_RATE = 10 #seconds
+
 with open("airports.txt") as f:
 	airports = f.readlines()
 airports = [x.strip() for x in airports]
@@ -19,39 +21,43 @@ print(airports)
 url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=5&mostRecentForEachStation=true&stationString=" + ",".join([item for item in airports if item != "NULL"])
 print(url)
 
-content = urllib.request.urlopen(url).read()
+while (True):
 
-# Retrieve flying conditions from the service response and store in a dictionary for each airport
-root = ET.fromstring(content)
-conditionDict = { "":"" }
-for metar in root.iter('METAR'):
-	stationId = metar.find('station_id').text
-	if metar.find('flight_category') is None:
-		print("Missing flight condition, skipping.")
-		conditionDict[stationId] = "Error"
-		#print("Missing flight condition for " + stationId)
-		continue
-	flightCategory = metar.find('flight_category').text
-	conditionDict[stationId] = flightCategory
-	#print("" + stationId + ": " + flightCategory)
+	content = urllib.request.urlopen(url).read()
 
-pixels = neopixel.NeoPixel(board.D18, len(conditionDict))
+	# Retrieve flying conditions from the service response and store in a dictionary for each airport
+	root = ET.fromstring(content)
+	conditionDict = { "":"" }
+	for metar in root.iter('METAR'):
+		stationId = metar.find('station_id').text
+		if metar.find('flight_category') is None:
+			print("Missing flight condition, skipping.")
+			conditionDict[stationId] = "Error"
+			#print("Missing flight condition for " + stationId)
+			continue
+		flightCategory = metar.find('flight_category').text
+		conditionDict[stationId] = flightCategory
+		#print("" + stationId + ": " + flightCategory)
 
-for i, airportCode in enumerate(airports):
-	if conditionDict[airportCode] != "No":
-		if conditionDict[airportCode] == "VFR":
-			color = COLOR_VFR
-		elif conditionDict[airportCode] == "MVFR":
-			color = COLOR_MVFR
-		elif conditionDict[airportCode] == "IFR":
-			color = COLOR_IFR
-		elif conditionDict[airportCode] == "LIFR":
-			color = COLOR_LIFR
-		else:
-			color = COLOR_ERROR
+	pixels = neopixel.NeoPixel(board.D18, len(conditionDict))
 
-	pixels[i] = color
-	print ("" + airportCode + ": " + conditionDict[airportCode] + ", " + flightCategory)
+	for i, airportCode in enumerate(airports):
+		if conditionDict[airportCode] != "No":
+			if conditionDict[airportCode] == "VFR":
+				color = COLOR_VFR
+			elif conditionDict[airportCode] == "MVFR":
+				color = COLOR_MVFR
+			elif conditionDict[airportCode] == "IFR":
+				color = COLOR_IFR
+			elif conditionDict[airportCode] == "LIFR":
+				color = COLOR_LIFR
+			else:
+				color = COLOR_ERROR
+
+		pixels[i] = color
+		print ("" + airportCode + ": " + conditionDict[airportCode] + ", " + flightCategory)
+
+	time.sleep(REFRESH_RATE)
 
 
 
